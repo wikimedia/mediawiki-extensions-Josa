96c725a444ba43037369c99017ec5ffff4d6af28
paladox <thomasmulhall410@yahoo.com>
2015-05-14 20:47:20 +0200

    Add extension.json, empty PHP entry point
    Bug: T98823
    Change-Id: I5a1038b7b77618d993ac97653053530a5ac80f22

--- Josa.php
+++ Josa.php
@@ -1,29 +1,14 @@
 <?php
-/**
-  * Extension:Josa
-  * Author: JuneHyeon Bae <devunt@gmail.com>
-  * Original implementation by Park Shinjo <peremen@gmail.com>
-  * License: MIT License
-*/
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is a MediaWiki extension, it is not a vaild entry point' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'Josa' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Josa'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['JosaMagic'] = __DIR__ . '/Josa.i18n.magic.php';
+	wfWarn(
+		'Deprecated PHP entry point used for Josa extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the Josa extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'Josa',
-	'author' => 'JuneHyeon Bae (devunt)',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:Josa',
-	'descriptionmsg' => 'josa-desc',
-	'version'  => '0.1.1',
-	'license-name' => 'MIT',
-);
-$wgHooks['ParserFirstCallInit'][] = 'Josa::onParserFirstCallInit';
-
-// Load i18n files
-$wgMessagesDirs['Josa'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['JosaMagic'] = __DIR__ . '/Josa.i18n.magic.php';
-
-$wgAutoloadClasses['Josa'] = __DIR__ . '/Josa.class.php';
-$wgParserTestFiles[] = __DIR__ . '/tests/parserTests.txt';
