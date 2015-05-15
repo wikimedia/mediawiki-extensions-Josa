5579dd0bee631cc00e48f3851b9f3090d7b1d782
Kunal Mehta <legoktm@gmail.com>
2015-05-15 12:00:13 -0700

    Comment out deprecated PHP entry point warning for now
    Until Wikimedia production switches to wfLoadExtension() directly.
    
    Change-Id: I96d17f28181d9b21a20a4576fafb5384994b05d1

--- Josa.php
+++ Josa.php
@@ -4,10 +4,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Josa'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['JosaMagic'] = __DIR__ . '/Josa.i18n.magic.php';
-	wfWarn(
+	/* wfWarn(
 		'Deprecated PHP entry point used for Josa extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);
+	); */
 	return;
 } else {
 	die( 'This version of the Josa extension requires MediaWiki 1.25+' );
